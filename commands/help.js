b3f5c9d8dd4af0235277e8025dcda4127e600b53
Therealzz <45063024+Therealzz@users.noreply.github.com>
2024-06-24 18:31:26 +0800

    Update help.js banner image

--- commands/help.js
+++ commands/help.js
@@ -49,7 +49,7 @@ module.exports = {
           { name: '🔀 Shuffle', value: 'Shuffle the songs in queue' },
           { name: '📃 playlist', value: 'manage the playlists' }
         )
-        .setImage(`https://cdn.discordapp.com/attachments/1004341381784944703/1165201249331855380/RainbowLine.gif?ex=654f37ba&is=653cc2ba&hm=648a2e070fab36155f4171962e9c3bcef94857aca3987a181634837231500177&`); 
+        .setImage(`https://https://cdn.discordapp.com/attachments/1134921192323678258/1254745493985169429/2024-03-31_kb_banner.png?ex=667a9c4d&is=66794acd&hm=bc110f7bb8ba81cbd0d7e801a145bac002327a4cc91e8dac5b089b0f9f278fea&`); 
 
       const basicCommandsEmbed = new EmbedBuilder()
         .setColor(client.config.embedColor)
@@ -62,7 +62,7 @@ module.exports = {
            { name: '🎵 Now Playing', value: 'Display the currently playing song information' },
           { name: '🔊 Volume', value: 'Adjust the music volume [ hearing at high volumes is risky ]' },
         ) 
-       .setImage('https://cdn.discordapp.com/attachments/1150827819547504741/1168917372267151370/standard.gif?ex=65538222&is=65410d22&hm=b4994392f44679da41fc9304eb69deaa3769e136057556deec0db69ae8d33a97&')
+       .setImage('https://https://cdn.discordapp.com/attachments/1134921192323678258/1254745493985169429/2024-03-31_kb_banner.png?ex=667a9c4d&is=66794acd&hm=bc110f7bb8ba81cbd0d7e801a145bac002327a4cc91e8dac5b089b0f9f278fea&')
       const button1 = new ButtonBuilder()
         .setLabel('YouTube')
         .setURL('https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A')
